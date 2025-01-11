66f8c329fc8da7395d3365f5c3a5ceaaa2eaefd9
Masoumi <1014382@saipacorp.com>
2025-01-11 18:23:30 +0330

    refactor

--- src/_cloner/utils/axiosConfig.ts
+++ src/_cloner/utils/axiosConfig.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const http = axios.create({
-    baseURL: "http://localhost:7142/api/",
+    baseURL: "https://behtanroo.ir/api",
     headers: {
         "Content-Type": "application/json",
-        // "Authorization": 'Bearer '+Cookies.get('token')
+        // Authorization: `Bearer ${Cookies.get("token")}`,
+    }
+})
+export const httpFormData = axios.create({
+    baseURL: "https://behtanroo.ir/",
+    headers: {
+        "Content-Type": "application/json",
+        // Authorization: `Bearer ${Cookies.get("token")}`,
     }
 })
